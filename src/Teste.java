2ca0a1083058536cd28d69345cc4a785fa0d12c0
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-03-06 15:01:46 -0300

    Método anagrama inicial

--- src/Teste.java
+++ src/Teste.java
@@ -6,8 +6,29 @@ public class Teste {
 
 	}
 	
-	private static final int ASCII_CODES = 256;
+	private static final int CODIGO_ASCII = 256;
 	
-	
+	public static boolean anagrama(String palavraUm, String palavraDois) {
+		
+		int[] contador = new int[CODIGO_ASCII];
+		char[] caracterPalavraUm = palavraUm.toCharArray();
+		char[] caracterPalavraDois = palavraDois.toCharArray();
+		
+		if(caracterPalavraUm != caracterPalavraDois) {
+			return false;
+		}
+		
+		for(int i = 0; i < caracterPalavraUm.length; i++) {
+			contador[caracterPalavraUm[i]]++;
+			contador[caracterPalavraDois[i]]--;
+		}
+		
+		for(int i = 0; i < contador.length; i++) {
+			if(contador[i] != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
